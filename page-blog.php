6e30cf477599c1c59594ee2a765f64aae9db20e0
Smolmeri <paavolainent@gmail.com>
2019-10-16 14:23:17 +0300

    added header custom field

--- page-blog.php
+++ page-blog.php
@@ -1,10 +1,14 @@
 <?php
 
+// Custom Fields
+
+$main_header = get_field('main_header');
+
 get_header();
 ?>
 
 <div id="blog">
-    <h1 class="big-header"><?php echo $blog_header; ?></h1>
+    <h1 class="big-header"><?php echo $main_header; ?></h1>
     <div class="blog-cards-container">
         <?php $loop = new WP_Query(array('posts_per_page' => 4, 'post_type' => 'blog_posts', 'orderby' => 'post_id', 'order' => 'DESC')); ?>
         <?php while ($loop->have_posts()) : $loop->the_post(); ?>
