1db8f045156bc33d85e95e2cf016d99409fa9688
Smolmeri <paavolainent@gmail.com>
2019-10-17 12:24:23 +0300

    added wrapper around navbutton to make it stand out

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,47 +13,45 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/styles/index.css">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/styles/single.css">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/styles/mission.css">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/styles/team.css">
-	<link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<!-- <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wanderdicks' ); ?></a> -->
+    <div id="page" class="site">
+        <!-- <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'wanderdicks'); ?></a> -->
+        <div class="nav-wrapper"></div>
+            <div class="nav-container" onclick="onNavPress()">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+            </div>
+        
 
-	<div class="nav-container" onclick="onNavPress()">
-        <div class="bar1"></div>
-        <div class="bar2"></div>
-        <div class="bar3"></div>
-    </div>
-
-    <div class="nav-list">
+        <div class="nav-list">
             <img src="./assets/img/tempLogo.png" alt="asd">
-        <ul>
-            <a href="home">
-                <li>Home</li>
-            </a>
-            <a href="projects">
-                <li>Mission</li>
-            </a>
-            <a href="blog">
-                <li>Adventures</li>
-            </a>
-            <a href="team">
-                <li>Meet the Team</li>
-            </a>
-        </ul>
-    </div>
-
-
-
-   
+            <ul>
+                <a href="home">
+                    <li>Home</li>
+                </a>
+                <a href="projects">
+                    <li>Mission</li>
+                </a>
+                <a href="blog">
+                    <li>Adventures</li>
+                </a>
+                <a href="team">
+                    <li>Meet the Team</li>
+                </a>
+            </ul>
+        </div>
